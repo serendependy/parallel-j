de5b800d3ac9f1c2e3d2f10657632249ab77cfac
chris-genesi <chris.jenkins@genesi-usa.com>
2012-11-24 15:39:12 -0600

    Added that file where macrotypes were moved

---
+++ src/j/lang/datatypes/JTypeMacros.scala
@@ -0,0 +1,34 @@
+package j.lang.datatypes
+
+import j.util.CMacroType
+
+class JTypeMacros private ()
+
+object JTypeMacros {
+  
+	type JType = CMacroType[JTypeMacros]
+		  val List(jB01 , jLIT , jINT , jFL , jCMPX , jBOX , jXNUM, jRAT, jBIT,
+	           jSB01, jSLIT, jSINT, jSFL, jSCMPX, jSBOX, jSBT,
+	           jC2T , jVERB, jADV , jCONJ,jASGN , jMARK): List[JType] = (0 to 
+	               21).map((x:Int) => new JType(1 >> x))
+	  val List(jSYMB, jCONW, jNAME,
+	           jLPAR, jRPAR, jXD  , jXZ):List[JType] = (22 to 
+	               28).map((x:Int) => new JType(1 >> x))
+
+	  //composite j types
+	  val jANY = new JType(-1)
+	  val jSPARSE = jSB01 | jSINT | jSFL | jSCMPX | jSLIT | jSBOX
+	  val jNUMERIC = jB01 & jBIT | jINT | jFL | jCMPX | jXNUM | 
+	  		   jRAT | jSB01 | jSINT | jSFL | jSCMPX
+	  val jDIRECT = jLIT | jC2T | jB01 | jBIT | jINT | jFL | 
+	  		   jCMPX | jSBT
+	  val jCHAR = jLIT | jC2T | jSLIT
+	  val jNOUN = jNUMERIC | jCHAR | jBOX | jSBOX |
+	  		   jSBT
+	  val jDENSE  = jNOUN & (~jSPARSE)
+	  val jFUNC = jVERB | jADV | jCONJ
+	  val jRHS  = jNOUN | jFUNC
+	  val jIS1BYTE = jB01 | jLIT
+	  val jLAST0 = jB01 | jLIT | jC2T | jNAME
+  
+}
