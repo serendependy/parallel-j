b499603e84802b980b5bb12d1618749e6a45c83b
chris-genesi <chris.jenkins@genesi-usa.com>
2012-12-18 15:21:52 -0600

    Tried to begin implementing functions; realized I need a general numeric type for add.

---
+++ src/j/lang/primitives/JVerbs.scala
@@ -0,0 +1,11 @@
+package j.lang.primitives
+
+import j.lang.datatypes.JFuncRank
+import j.lang.datatypes.function.JVerb
+
+import j.lang.datatypes.array.{JArray, JArrayType}
+
+
+object JVerbs {
+
+}
