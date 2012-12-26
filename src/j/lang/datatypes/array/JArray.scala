5dc2e20cc90d9f1e0d21b2da75a0bea164d50284
christopher <cjenkin1@trinity.edu>
2012-12-26 02:59:17 -0600

    Still messing with type inference in scalar function.

--- src/j/lang/datatypes/array/JArray.scala
+++ src/j/lang/datatypes/array/JArray.scala
@@ -2,21 +2,22 @@ package j.lang.datatypes.array
 
 import j.lang.datatypes.JTypeMacros._
 import j.lang.datatypes.array.JArrayType._
-import j.lang.datatypes.array.types.numeric._
-import j.lang.datatypes.array.types.numeric.NumericImplicits._
-
+import j.lang.datatypes.array.types._
+import j.lang.datatypes.array.types.NumericImplicits._
 import JArrayType._
 import JArrayFlag._
-
 import j.util.Rational
+
+import JArray._
+import j.lang.datatypes.array.types.JNumber
 
 object JArray {
   
-  def apply[T: JArrayType](flag: JArrayFlag, jaType: JType, refcount: Int, 
+  def apply[T <% JArrayType : JArrayConstructType](flag: JArrayFlag, jaType: JType, refcount: Int, 
       numItems: Int, shape: List[Int], ravel: Array[T]) = 
         new JArray(flag, jaType, refcount, numItems, shape, ravel)
   
-  def scalar[T: JArrayType : Manifest](sc: T): JArray[T] = {
+  def scalar[T <% JArrayType : JArrayConstructType : Manifest](sc: T): JArray[T] = {
     val myTypeMacro = sc match { // should have exhaustiveness check, but doesn't b/c nesting
       case x:Byte	=> jB01
       case x:Int 	=> jINT
@@ -24,10 +25,10 @@ object JArray {
       case x:Char	=> jCHAR
       case x:Rational=> jXNUM
     }
-    JArray(afNONE, myTypeMacro,0, 1, List(), Array(sc))
+    JArray(afNONE, myTypeMacro,0, 1, List(), Array[T](sc))
   }
     
-    def arithmeticProgression(n: Int, b: Int, m: Int):JArray[JNumber] =
+    def arithmeticProgression(n: Int, b: Int, m: Int):JArray[JInt] =
       JArray(afNONE, jINT, 0, n, List(n), (0 to n).map(b + m * _).toArray)
 
     def string(str: String) = {
@@ -42,15 +43,13 @@ object JArray {
       JArray(afNONE, jINT, 0, 1, List(1), Array(a))
     }
 
-  val zero = scalar(0)
+  val zero:JArray[JInt] = scalar(0)
   val one  = scalar(1)
   val two  = scalar(2)
   val mone = scalar(-1)
   val pi   = scalar(scala.Math.Pi)
 }
-
-import JArray._
-class JArray[T: JArrayType](val flag: JArrayFlag, val jaType: JType, 
+class JArray[T <% JArrayType](val flag: JArrayFlag, val jaType: JType, 
     var refcount: Int, val numItems: Int, val shape: List[Int], 
     val ravel: Array[T]) {
 
--- src/j/lang/datatypes/array/JArrayType.scala
+++ src/j/lang/datatypes/array/JArrayType.scala
@@ -1,19 +1,3 @@
 package j.lang.datatypes.array
 
-import j.util.CMacroType
-import j.util.Rational
-
-import j.lang.datatypes.array.types.numeric.JNumber
-
-sealed class JArrayType[T]
-
-object JArrayType {
-
-	  //the three type families in J
-	  implicit object JCHAR		extends JArrayType[Char]
-
-	  class Box[T: JArrayType](val value: T)
-	  implicit object JBOX	extends JArrayType[Box[_]]
-	  
-	  implicit object JNUMERIC extends JArrayType[JNumber]
-}
+class JArrayType // the top of the J type hierarchy
