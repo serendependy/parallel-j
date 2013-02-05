e46b3732dfacf3b2c8591dc9f9cf76ab9e040485
christopher <cjenkin1@trinity.edu>
2013-02-05 12:39:18 -0600

    Added functionality to assist functions in creating appropriate array frames

--- src/j/lang/datatypes/JFuncRank.scala
+++ src/j/lang/datatypes/JFuncRank.scala
@@ -2,10 +2,11 @@ package j.lang.datatypes
 
 import j.lang.datatypes.array.types.JNumberTypes._
 
-class JFuncRank(r1: JNumber, r2: JNumber, r3: JNumber)
+class JFuncRank(val r1: JNumber, val r2: JNumber, val r3: JNumber)
 
 object JFuncRank {
 	def apply[N <% JNumber](r1: N, r2: N, r3: N) = new JFuncRank(r1,r2,r3)
-	def apply[N <% JNumber](r: N) = new JFuncRank(r, r, r)
+	def apply[N <% JNumber](r2: N, r3: N) 		 = new JFuncRank(r3,r2,r3) 
+	def apply[N <% JNumber](r: N) 				 = new JFuncRank(r, r, r)
 }
 
--- src/j/lang/datatypes/array/JArrayFrame.scala
+++ src/j/lang/datatypes/array/JArrayFrame.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable.Stack
 
 import j.lang.datatypes.array.types.JNumberTypes._
 import j.lang.datatypes.array.JArrayFlag._
+import j.lang.datatypes.JFuncRank
 
 object JArrayFrame {
   def apply(frameLevels: List[JNumber], jar: JArray[_]) = {
@@ -15,6 +16,11 @@ object JArrayFrame {
   }
   
   def apply(rank: JNumber, jar: JArray[_]): JArrayFrame = JArrayFrame(List(rank), jar)
+  
+  def createFrames(rs: List[JFuncRank], jar: JArray[_]) = JArrayFrame(rs.map(_ r1), jar)
+  def createFrames(rs: List[JFuncRank], jar1: JArray[_], jar2: JArray[_]): (JArrayFrame, JArrayFrame) = {
+    (JArrayFrame(rs.map(_ r2), jar1), JArrayFrame(rs.map(_ r3), jar2))
+  }
 }
 
 class JArrayFrame private(val frameLevels: List[Int], val jar: JArray[_]) {
@@ -55,59 +61,3 @@ class JArrayFrame private(val frameLevels: List[Int], val jar: JArray[_]) {
 	  "" + frames + "\n" + jar
 	}  
 }
-//	      )
-//	    )
-//	    }
-//	  }
-//	  case _ => throw new Exception() //TODO domain error
-//	}
-//	
-//	def apply[T <% JArrayType : Manifest](itemRanks: List[JNumber], jar: JArray[T]):JArrayFrame = {
-//	  if (itemRanks isEmpty) JArrayFrame(jar) else
-//	  itemRanks(0) match {
-//	    case inf: JInfinite => new JArrayFrame(List(), Array(Right(jar)))
-//	    case intRank: JInt => {
-//	      if (intRank.v >= jar.rank)
-//	        new JArrayFrame(List(), Array(Right(jar)) )
-//	      else {
-//	    	  val cellShape = jar.shape.drop(jar.rank - intRank.v)
-//	    	  val numInCells = cellShape.fold(1)(_ * _)
-//	    	  val frameShape = jar.shape.take(jar.rank - intRank.v)
-//	    	  val numInFrame = frameShape.fold(1)(_ * _)
-//	    	  new JArrayFrame(frameShape, Array.tabulate(numInFrame)(
-//	    		 (i: Int) => {
-//	    		   val mjar = JArray(afNONE, jar.jaType, numInCells, 0, cellShape,
-//	                    Array.tabulate(numInCells)((j: Int) => jar.ravel((i * numInCells) + j)))
-//	    		   if (itemRanks.length == 1)
-//	    		     Right(mjar)
-//	               else
-//	                 Left(JArrayFrame(itemRanks.drop(1), mjar))
-//	             }
-//	          ))
-//	      }
-//	    }
-//	    case _ => throw new Exception() //TODO make domain error
-//	  }
-//	}
-//	
-//	def apply[T <% JArrayType : Manifest](jar: JArray[T]) = new JArrayFrame(List(), Array(Right(jar)))
-//	
-//}
-//
-//class JArrayFrame(val frameShape: List[Int], val cells: Either[Array[JArrayFrame], Array[JArray[_]]]) {
-//	val numFrames = cells.fold(_ length, _ length)
-//	val cellShape = cells match {
-//	  case Left(ajaf) => 
-//	}
-////	val cellShape = if (cells isEmpty) List() else cells(0) match {
-////	  case Left(jaf) => jaf.numFrames
-////	  case Right(jar)=> jar.shape
-////	}
-//	
-//	override def toString() = {
-//	  "" + frameShape + "\n"+ cellShape + "\n" + cells.map(_ match {
-//	    case Left(jaf) => jaf.toString()
-//	    case Right(jar)=> jar.toString()
-//	  }).mkString("\n----\n")
-//	}
-//}
