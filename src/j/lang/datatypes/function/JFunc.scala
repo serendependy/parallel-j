b8ecdf90adbd37f6c8fbb3c6c43652dd0da7cfd6
christopher <cjenkin1@trinity.edu>
2013-03-05 19:20:18 -0600

    No more unsightly errors

--- src/j/lang/datatypes/function/JFunc.scala
+++ src/j/lang/datatypes/function/JFunc.scala
@@ -7,8 +7,8 @@ import j.lang.datatypes.array._
 
 import j.util.CMacroType._
 
-abstract class JFunc[M <% JDataType, D1 <% JDataType, D2 <% JDataType,
-    MR <% JDataType, DR <% JDataType](
+abstract class JFunc[M <: JDataType, D1 <: JDataType, D2 <: JDataType,
+    MR <: JDataType, DR <: JDataType](
     val rep: String, val funcType: JTypeMacro, 
 	val mdomain: JTypeMacro, val d1domain: JTypeMacro, val d2domain: JTypeMacro) {
 
@@ -18,26 +18,29 @@ abstract class JFunc[M <% JDataType, D1 <% JDataType, D2 <% JDataType,
   val d1InDomain= inDomain(d1domain)_
   val d2InDomain= inDomain(d2domain)_
   
-  protected def monadImpl[TR <: MR, T <: M <% TR](y: T): TR
-  protected def dyadImpl[T1 <: D1, T2 <: D2](x: T1, y: T2): DR
+  def monad[T <: M](y: T): MR
+  def dyad[T1 <: D1, T2 <: D2](x: T1, y: T2): DR
   
-  def monad[T <: M <% MR](y: T):MR = {
-    if (mInDomain(y.jtype) ) {
-      monadImpl(y)
-    }
-    else {
-      throw new Exception() //TODO make this a domain error
-    }
-  }
+//  protected def monadImpl[TR <: MR, T <: M <% TR](y: T): TR
+//  protected def dyadImpl[T1 <: D1, T2 <: D2](x: T1, y: T2): DR
   
-  def dyad(x: D1, y: D2): DR = {
-    if (d1InDomain(x.jtype) && d2InDomain(x.jtype)) {
-      dyadImpl(x,y)
-    }
-    else {
-      throw new Exception() //TODO make this domain error
-    }
-  }
+//  def monad[T <: M <% MR](y: T):MR = {
+//    if (mInDomain(y.jtype) ) {
+//      monadImpl(y)
+//    }
+//    else {
+//      throw new Exception() //TODO make this a domain error
+//    }
+//  }
+//  
+//  def dyad(x: D1, y: D2): DR = {
+//    if (d1InDomain(x.jtype) && d2InDomain(x.jtype)) {
+//      dyadImpl(x,y)
+//    }
+//    else {
+//      throw new Exception() //TODO make this domain error
+//    }
+//  }
 }
 
 object JFunc {
