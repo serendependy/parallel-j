33ca7b1b469b0989ecf524bf9dd74d9c5104e73d
chris-genesi <chris.jenkins@genesi-usa.com>
2013-03-31 23:59:55 -0600

    Added scala solution to problem list. That still needs fixing.

---
+++ thesisPaper/num_int_pi.scala
@@ -0,0 +1,18 @@
+/*Imports here*/
+
+// TODO this is the wrong method!
+
+def testNumericalIntegration(numRiemann: JArray[JInt]) {
+   val recip =  reciprocalDivide(numRiemann)//JArray.scalar(numRiemann.recip)
+   val xvals =  signumMultiply(integersIndex(numRiemann),
+                               recip)
+   val yvals = squarerootRoot(negateMinus(JArray.scalar(JReal.One),
+                              squareNotand(xvals)))
+   val pi = signumMultiply(
+      signumMultiply(
+	     (conjugatePlus insert).apply(yvals),
+	      JArray.scalar[JInt,Int](4)),
+      recip)
+
+   println("PI is: " + pi)
+}
